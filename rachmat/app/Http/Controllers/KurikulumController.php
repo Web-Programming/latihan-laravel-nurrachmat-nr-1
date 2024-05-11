b6743fc1b4ac4ab8f986c556ba69fd86e873fe9a
UMDP <rachmat.nur91@gmail.com>
2024-05-11 15:02:23 +0700

    MCR

---
+++ rachmat/app/Http/Controllers/KurikulumController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kurikulum;
+use Illuminate\Http\Request;
+
+class KurikulumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Kurikulum $kurikulum)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Kurikulum $kurikulum)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Kurikulum $kurikulum)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Kurikulum $kurikulum)
+    {
+        //
+    }
+}
