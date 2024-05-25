a134bc196b44ab42940029481334566911ba0587
Nur Rachmat <rachmat.nur91@gmail.com>
2024-05-25 14:32:58 +0700

    Auth controller : membuat method login

---
+++ rachmat/app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    //halaman login
+    public function index() {
+        //ambil data use simpan di variable $user
+        $user = Auth::user();
+
+        //jika ada user (user telah logged in)
+        if($user){
+            //jika user memiliki level admin
+            if($user->level == 'admin'){
+                return redirect()->intended('admin');
+            }
+            //jika user memiliki level user
+            else if($user->level == 'user'){
+                return redirect()->intended('user');
+            }
+        }
+
+        return view('login');
+    }
+
+    public function proses_login(Request $request){
+        //form validation
+        $request->validate(
+            [
+                'username' => 'required',
+                'password' => 'required'
+            ]
+        );
+        //ambil data dari $request, username dan password saja
+        $credential = $request->only('username', 'password');
+        //proses cek login di tabel users
+        if(Auth::attempt($credential)){
+            //jika login berhasil
+            $user = Auth::user();
+            if($user->level == 'admin'){
+                return redirect()->intended('admin');
+            }else if($user->level == 'user'){
+                return redirect()->intended('user');
+            }
+            return redirect()->intended('/');
+        }
+
+        //jika login gagal, arahkan ke halaman login
+        //gunakan flash data untuk menampilkan error
+        return redirect('login')
+            ->withInput()
+            ->withErrors(
+                ['login_gagal' => 'User tidak terdaftar (email atau password salah)!']
+            );
+    }
+}
