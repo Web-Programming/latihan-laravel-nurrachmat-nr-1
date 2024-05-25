3a38174fc560d86ed624c75b638f72af7d5f89be
Nur Rachmat <rachmat.nur91@gmail.com>
2024-05-25 14:15:38 +0700

    Update Kernel.php

--- rachmat/app/Http/Kernel.php
+++ rachmat/app/Http/Kernel.php
@@ -64,6 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         //tambahkan middleware alias dengan nama cek_login
-        "cek_login" => \App\Http\Middleware\CekLogin
+        "cek_login" => \App\Http\Middleware\CekLogin::class
     ];
 }
